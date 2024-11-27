0e5f688c25461e9b393020a33bd65234492e3980
Vinita Hingonekar <hingonekarvinita@gmail.com>
2024-11-27 23:05:31 +0530

    Added keys and doors

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public KeyController keyController;
+    public DoorController door1;
+    public DoorController door2;
+
+    public GameObject levelCompletePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(KeyController.keysCollected == 2)
+        {
+            if(door1.isPlayerAtDoor && door2.isPlayerAtDoor)
+            {
+                Debug.Log("level completed");
+                StartCoroutine("LevelComplete");
+            }
+        }
+    }
+
+    private IEnumerator LevelComplete()
+    {
+        yield return new WaitForSeconds(1f);
+        levelCompletePanel.SetActive(true);
+    }
+}
